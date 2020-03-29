9daab0db8f7ad8154344019c9e07163ccba94db6
mana.tanaka <mana.tanaka@accenture.com>
2020-03-29 16:05:43 +0900

    scssの道入

--- pages/index.vue
+++ pages/index.vue
@@ -81,12 +81,15 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
 export default {
   components: {
     Logo,
-    VuetifyLogo
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
+
